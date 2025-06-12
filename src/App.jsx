6877dc4d540947a60ab65e19ef14535065531d70
Celeste <celeste.ipn92@gmail.com>
2025-06-11 19:20:06 -0600

    se agregaron los codigos correspondientes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card-container">
+      <div className="profile-card">
+        <img
+          src="https://i.imgur.com/uXQF5gD.png" // Puedes cambiar la imagen por otra que te guste
+          alt="Foto de Celeste"
+          className="profile-img"
+        />
+        <h1>Celeste Vianey Hernández Arauz</h1>
+        <h2>Estudiante de la especialidad en Mercadotecnia & Frontend Dev</h2>
+        <p>Apasionada por la tecnología, el ámbito empresarial y el aprendizaje continuo 💻</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
